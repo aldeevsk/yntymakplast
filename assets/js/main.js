32569f7ae6d744c10b1a41d712c82443eedebe38
aldeevsk <59862247+aldeevsk@users.noreply.github.com>
2022-12-19 18:40:50 +0600

    click on input

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,55 +12,11 @@ import {initForms} from './forms.js'
 })()
 
 window.addEventListener('DOMContentLoaded', () => {
-    const input = document.querySelector('input[type=tel]')
-
-    s/* etTimeout(() => {
-        const event = document.createEvent('TouchEvent')
+    setTimeout(() => {
+        document.querySelector('input[type=tel]').focus()
+        const event = document.createEvent('touchstart')
         document.querySelector('input[type=tel]').dispatchEvent(event)
-    }, 1000); */
-
-    simulateTouch = function () {
-      var param = {
-        type: 'touchstart',
-        canBubble: true,
-        cancelable: true,
-        view: window,
-        detail: 0,
-        screenX: 0,
-        screenY: 0,
-        clientX: 0,
-        clientY: 0,
-        ctrlKey: false,
-        altKey: false,
-        shiftKey: false,
-        metaKey: false,
-        touches: 0,
-        targetTouches: 0,
-        changedTouches: 0,
-        scale: 0,
-        rotation: 0,
-        touchItem: 0,
-      }
-
-      var event = document.createEvent('TouchEvent')
-
-      event.initTouchEvent(
-        param.touchItem,
-        param.touchItem,
-        param.touchItem,
-        param.type,
-        param.view,
-        param.screenX,
-        param.screenY,
-        param.clientX,
-        param.clientY,
-        param.ctrlKey,
-        param.altKey,
-        param.shiftKey,
-        param.metaKey
-      )
-
-      input.dispatchEvent(event)
-    }
+    }, 1000);
+    
 
 })
