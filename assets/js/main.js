d2c0652d67a462887f644cbd23282206de9b40bc
aldeevsk <59862247+aldeevsk@users.noreply.github.com>
2022-12-19 19:35:36 +0600

    click on input

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,12 +11,43 @@ import {initForms} from './forms.js'
     initForms('form')
 })()
 
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        document.querySelector('input[type=tel]').focus()
-        const event = document.createEvent('touchstart')
-        document.querySelector('input[type=tel]').dispatchEvent(event)
-    }, 1000);
-    
+/* window.addEventListener('DOMContentLoaded', () => {
+        const input = document.querySelector('input[type=tel]')
+        
+        
+        input.addEventListener('click', event => {
+          const e = document.createEvent('TouchEvent')
+          e.initTouchEvent('touchstart', true, true)
+          console.log(event.target)
+        })
+        input.click()
+}) */
 
+/* if (Touch.length < 1 || TouchEvent.length < 1)
+  throw 'TouchEvent constructors not supported' */
+const input = document.querySelector('input[type=tel]')
+var touch = new Touch({
+  identifier: 42,
+  target: input,
+  clientX: 200,
+  clientY: 200,
+  screenX: 300,
+  screenY: 300,
+  pageX: 200,
+  pageY: 200,
+  radiusX: 5,
+  radiusY: 5,
 })
+
+var touchEvent = new TouchEvent('touchstart', {
+  cancelable: true,
+  bubbles: true,
+  composed: true,
+  touches: [touch],
+  targetTouches: [touch],
+  changedTouches: [touch],
+})
+
+input.dispatchEvent(touchEvent)
+
+/* input.addEventListener('touchstart', event => console.log('touch')) */
